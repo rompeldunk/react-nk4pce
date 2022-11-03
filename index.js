6c2cb25f5c614084b15a66339b3523a494ec7b94
rompeldunk <magnus.gule@gmail.com>
2022-11-03 11:44:26 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import { StyledEngineProvider } from '@mui/material/styles';
+import SettingsDialog from './SettingsDialog';
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
   <React.StrictMode>
